7e9a3db41e94ec337a4fdcd4f24def3da1d50263
Yunusali <37039084+Khakimjanovich@users.noreply.github.com>
2022-03-14 00:34:24 +0500

    initial commit

---
+++ db/DbModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\core\db;
+
+use app\core\Application;
+use app\core\Model;
+use PDOStatement;
+
+abstract class DbModel extends Model
+{
+    public static function findOne($where)
+    {
+        $tableName = static::tableName();
+        $attributes = array_keys($where);
+        $sql = implode("AND", array_map(fn($attr) => "$attr = :$attr", $attributes));
+        $statement = self::prepare("SELECT * FROM $tableName WHERE $sql");
+        foreach ($where as $key => $item) {
+            $statement->bindValue(":$key", $item);
+        }
+        $statement->execute();
+        return $statement->fetchObject(static::class);
+    }
+
+    abstract public static function tableName(): string;
+
+    public static function prepare($sql): PDOStatement
+    {
+        return Application::$app->db->prepare($sql);
+    }
+
+    public function primaryKey(): string
+    {
+        return 'id';
+    }
+
+    public function save(): bool
+    {
+        $tableName = $this->tableName();
+        $attributes = $this->attributes();
+        $params = array_map(fn($attr) => ":$attr", $attributes);
+        $statement = self::prepare("INSERT INTO $tableName (" . implode(",", $attributes) . ") 
+                VALUES (" . implode(",", $params) . ")");
+        foreach ($attributes as $attribute) {
+            $statement->bindValue(":$attribute", $this->{$attribute});
+        }
+        $statement->execute();
+        return true;
+    }
+}
